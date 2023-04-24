75ec59b13ca2d486835e924d3a87efc5ae112b79
Heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2023-04-24 21:40:02 +0800

    Add options page (in the form of user conrol) and options control template(experimental)

---
+++ HybridAI/Pages/OptionsPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HybridAI.Pages
+{
+    /// <summary>
+    /// OptionsPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class OptionsPage : UserControl
+    {
+
+        public static readonly RoutedEvent GoBackEvent = EventManager.RegisterRoutedEvent("GoBack", RoutingStrategy.Direct, typeof(RoutedEventArgs), typeof(OptionsPage));
+        public event RoutedEventHandler GoBack
+        {
+            add
+            {
+                AddHandler(GoBackEvent, value);
+            }
+            remove
+            {
+                RemoveHandler(GoBackEvent, value);
+            }
+        }
+        public OptionsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Back(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(GoBackEvent, this));
+        }
+    }
+}
